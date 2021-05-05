af9f21367442202a04239cdc855354341ffe4387
Jessica Bakare <69154035+jessiBak@users.noreply.github.com>
2021-05-05 16:23:13 -0400

    Added character + some talent ascension materials to CharacterCard

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default function CharacterCard(props)
 {
+    console.log("props.info: ", props.info);
     return(
         <div className="card flex-row">
             <img className="card-img-left chara-card-img" src={ props.info.img_src } alt={ props.info.name }/>
@@ -9,7 +10,28 @@ export default function CharacterCard(props)
                 <h4>{ props.info.name }</h4>
                 <p>Element: { props.info.element }</p>
                 <p>Weapon: { props.info.weapon }</p>
+                <DropdownList name="Character Asension Costs" list={ props.info.char_ascension_costs } />
+                <DropdownList name="Talent Ascension Costs" list={props.info.talent_ascension_costs.from_domains } />
                 <p>Character Ascension Materials Available Today? { String(props.info.talent_avail_today).charAt(0).toUpperCase() + String(props.info.talent_avail_today).slice(1) }</p>
+            </div>
+        </div>
+    );
+}
+
+function DropdownList(props)
+{
+    let lst = [];
+    for(let i = 0; i < props.list.length; i++)
+    {
+        lst.push((<a class="dropdown-item" href="#">{ String(props.list[i]) }</a>));
+    }
+    return(
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                { props.name }
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                { lst }
             </div>
         </div>
     );
--- src/CharacterNav.js
+++ src/CharacterNav.js
@@ -6,7 +6,7 @@ export default function CharacterNav(props)
     for(let i = 0; i < props.team.length; i++)
     {
         let src = "/images/" + props.team[i] + ".png";
-        console.log(`i = ${i}, props.team[i] = ${props.team[i]} `);
+        //console.log(`i = ${i}, props.team[i] = ${props.team[i]} `);
         btns.push(<button className="btn btn-secondary" id={props.team[i] + "-chara-nav-btn"} value={ props.team[i] } key= { i } onClick={ props.navClick } >
                     <img className="chara-nav-img" src={ src } alt={ props.team[i] } /></button>);
     }
