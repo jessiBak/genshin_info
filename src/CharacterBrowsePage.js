a11f93ce9c251f775e397f63ad9439191c943774
Jessica Bakare <69154035+jessiBak@users.noreply.github.com>
2021-05-09 17:32:21 -0400

    Fixed issues with character browsing carousel (mostly)

--- src/CharacterBrowsePage.js
+++ src/CharacterBrowsePage.js
@@ -11,35 +11,27 @@ export default function CharacterBrowsePage(props)
     {
         if(i === 0)
         {
-            allCards.push(<div class="carousel-item active"><CharacterCard info={allData[characterList[i]]} /></div>);
+            allCards.push(<div class="carousel-item col-md-4 active chara-browse-card"><CharacterCard info={allData[characterList[i]]} /></div>);
         }
         else
         {
-            allCards.push(<div class="carousel-item"><CharacterCard info={allData[characterList[i]]} /> </div>);
+            allCards.push(<div class="carousel-item col-md-4 chara-browse-card"><CharacterCard info={allData[characterList[i]]} /> </div>);
         }   
     }
 
     return(
-        <div className="container-fluid chara-browse-page">
-            <div id="carouselExampleControls" class="carousel slide" data-mdb-ride="carousel">
-                <div class="carousel-inner">
+        <div className="container-fluid chara-browse-page justify-content-center">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner row w-100 mx-auto">
                     { allCards }
                 </div>
-                <button
-                class="carousel-control-prev"
-                type="button"
-                data-mdb-target="#carouselExampleControls"
-                data-mdb-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
+                <button class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="false"></span>
+                <span class="sr-only">Previous</span>
                 </button>
-                <button
-                class="carousel-control-next"
-                type="button"
-                data-mdb-target="#carouselExampleControls"
-                data-mdb-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+                <button class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="false"></span>
+                <span class="sr-only">Next</span> 
                 </button>
             </div>
         </div>    
